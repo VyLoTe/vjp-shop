914c994e7041ee6c61fbf46022e1726a393f91ca
VyLoTe <antran064969@gmail.com>
2025-06-20 00:49:26 +0700

    header and footer

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,83 @@
+    <footer>
+        <div class="footer-wrapper">
+            <div class="footer-separator"></div>
+        </div>
+        <div class="site-footer">
+            <div class="top">
+                <div class="footer-section">
+                    <div class="name-section">Dịch vụ khách hàng</div>
+                    <ul>
+                        <li>Trung tâm trợ giúp Shopee</li>
+                        <li>Đơn hàng</li>
+                        <li>Trả hàng/Hoàn tiền</li>
+                        <li>Shopee xu</li>
+                    </ul>
+                </div>
+                <div class="footer-section">
+                    <div class="name-section">Thanh toán</div>
+                    <ul class="first-ul">
+                        <li>
+                            <a href=""><img src="public/images/visa.png" alt="logo"></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/spay.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/spaylater.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/tragop.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/cod.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/ae.png" alt=""></a>
+                        </li>
+                    </ul>
+                    <div class="name-section">Đơn vị vận chuyển</div>
+                    <ul class="first-ul">
+                        <li>
+                            <a href=""><img src="public/images/spx.png" alt="logo"></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/viettel.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/ninjavan.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href=""><img src="public/images/ahamove.png" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-section">
+                    <div class="name-section">Theo dõi</div>
+                    <ul>
+                        <li class="item">
+                            <a href="https://www.facebook.com/?ref=homescreenpwa">
+                                <img class="flo-icon" src="public/images/fb.png" alt="">
+                                <span class="name-icon">Facebook</span>
+                            </a>
+                        </li>
+                        <li class="item">
+                            <a href="https://github.com/VyLoTe">
+                                <img class="flo-icon" src="public/images/git.png" alt="">
+                                <span class="name-icon">Github</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-section">
+                    <div class="name-section">Tải ứng dụng VipShop</div>
+                    <div class="download">
+                        <img src="public/images/qr.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="bottom"></div>
+        </div>
+    </footer>
+    </body>
+
+    </html>
---
+++ app/Views/layouts/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>VIP Shop</title>
+    <link rel="stylesheet" href="public/css/header.css">
+    <link rel="stylesheet" href="public/css/footer.css">
+</head>
+
+<body>
+    <header>
+        <div class="navbar-wrapper">
+            <div class="left">
+                <div class="download-app"><a href="#">Tải ứng dụng</a></div>
+                <div class="connect"><a href="#">Kết nối</a></div>
+            </div>
+            <div class="right">
+                <div class="notification"><a href="#">Thông báo</a></div>
+                <div class="assist"><a href="#">Hỗ trợ</a></div>
+                <div class="account"><a href="#">User</a></div>
+            </div>
+        </div>
+        <div class="header-with-search">
+            <div class="logo-shop">
+                <img src="./public/images/logo_shop_icon.png" alt="Shop Logo" />
+                <div class="shop-name">VIPshop</div>
+            </div>
+            <div class="search-bar">
+                <form action="#" class="search-section">
+                    <div class="search-input">
+                        <input type="text" placeholder="Ưu đãi độc quyền" />
+                        <button type="button" class="search-button">
+                            <img src="./public/images/search-icon.png" alt="Search" />
+                        </button>
+                    </div>
+                </form>
+                <div class="recommend">
+                    <a href="#">rec1</a>
+                    <a href="#">rec2</a>
+                    <a href="#">rec3</a>
+                    <a href="#">rec4</a>
+                    <a href="#">rec5</a>
+                    <a href="#">rec6</a>
+                </div>
+            </div>
+            <div class="logo-cart">
+                <img src="./public/images/logo_cart.png" alt="Cart" />
+            </div>
+        </div>
+    </header>
