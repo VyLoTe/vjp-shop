8cb903bb91d046dcf91b0b652f28b5150e24dfd8
VyLoTe <antran064969@gmail.com>
2025-07-02 21:37:34 +0700

    more pics

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,11 +7,23 @@
 		<div class="container">
 			<div class="d-flex justify-content-center align-items-center" style="height: 270px; overflow: hidden;">
 				<div id="homeCarousel" class="carousel slide w-100 h-100" data-bs-ride="carousel">
-					<!-- Chấm tròn indicators -->
 					<div class="carousel-indicators">
 						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
 						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="8" aria-label="Slide 9"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="9" aria-label="Slide 10"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="10" aria-label="Slide 11"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="11" aria-label="Slide 12"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="12" aria-label="Slide 13"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="13" aria-label="Slide 14"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="14" aria-label="Slide 15"></button>
+						<button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="15" aria-label="Slide 16"></button>
 					</div>
 
 					<div class="carousel-inner w-100 h-100">
@@ -24,14 +36,62 @@
 						<div class="carousel-item w-100 h-100">
 							<img src="public/images/g3.jpg" class="d-block w-100 h-100 img-cover" alt="...">
 						</div>
+						<div class="carousel-item active w-100 h-100">
+							<img src="public/images/g4.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g5.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g6.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g7.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g8.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item active w-100 h-100">
+							<img src="public/images/g9.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g10.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g11.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item active w-100 h-100">
+							<img src="public/images/g12.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g13.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g14.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g15.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
+						<div class="carousel-item w-100 h-100">
+							<img src="public/images/g16.jpg" class="d-block w-100 h-100 img-cover" alt="...">
+						</div>
 					</div>
 
-					<button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+
+					<button class="carousel-control-prev custom-btn" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
 						<span class="carousel-control-prev-icon"></span>
 					</button>
-					<button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+					<button class="carousel-control-next custom-btn" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
 						<span class="carousel-control-next-icon"></span>
 					</button>
+				</div>
+				<div class="static-banner d-flex flex-column flex-grow-1 flex-shrink-1">
+					<div>
+						<img src="" alt="">
+					</div>
+					<div>
+						<img src="" alt="">
+					</div>
 				</div>
 			</div>
 		</div>
