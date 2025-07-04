ce33cbcff0904420f2c2ebdc098689e65062b26a
VyLoTe <antran064969@gmail.com>
2025-07-05 04:02:16 +0700

    anything goes

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>VIP Shop</title>
-    <link rel="stylesheet" href="public/css/header.css">
     <link rel="stylesheet" href="public/css/bootstrap5/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/home.css">
     <link rel="stylesheet" href="public/css/header.css">
